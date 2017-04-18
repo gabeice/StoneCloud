fb41ef2e3477f0887f861f1e60d9e1dd8e00b8b6
tacitblue <gabrielmcdonald1@gmail.com>
2017-04-18 17:12:42 -0400

    Capitalize m

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MastheadContainer from './masthead/masthead_container';
+import MastheadContainer from './Masthead/masthead_container';
 
 const App = ({ children }) => (
   <div>
