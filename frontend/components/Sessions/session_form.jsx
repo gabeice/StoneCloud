2ef5a4c7dc3861a8170388f22afff079a4eb9154
tacitblue <gabrielmcdonald1@gmail.com>
2017-04-18 14:52:45 -0400

    Auth works

--- frontend/components/Sessions/session_form.jsx
+++ frontend/components/Sessions/session_form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, hashHistory } from 'react-router';
 
 class SessionForm extends React.Component {
 	constructor(props) {
@@ -9,20 +9,37 @@ class SessionForm extends React.Component {
 			password: ""
 		};
     this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleName = this.handleName.bind(this);
+		this.handlePass = this.handlePass.bind(this);
   }
 
   handleSubmit(e) {
    e.preventDefault();
    const user = Object.assign({}, this.state);
-   this.props.processForm(user).then(() => this.redirect());
+   this.props.processForm(user).then(() => hashHistory.push("/"));
  }
 
+  handleName(e) {
+    e.preventDefault();
+    this.setState({username: e.target.value});
+  }
+
+  handlePass(e) {
+    e.preventDefault();
+    this.setState({password: e.target.value});
+  }
 
   render() {
     return(
       <div>
         <h2>{this.props.actionType}</h2>
-        <Link to={this.props.formType}>{this.props.altAction}</Link>
+        <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.username} onChange={this.handleName}/>
+        <br/>
+        <input type="text" value={this.state.password} onChange={this.handlePass}/>
+				<br/>
+        <input type="submit"/>
+        </form>
         <ul>{this.props.errors.map((err, idx) => <li key={idx}>{err}</li>)}</ul>
       </div>
     );
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,15 +4,32 @@ import { Provider } from 'react-redux';
 import App from './App'
 import SessionFormContainer from './Sessions/session_form_container';
 
-const Root = ({ store }) => (
-  <Provider store={ store }>
-    <Router history={ hashHistory }>
-      <Route path="/" component={ App }>
-        <Route path="/login" component={ SessionFormContainer } />
-        <Route path="/signup" component={ SessionFormContainer } />
-      </Route>
-    </Router>
-  </Provider>
-)
+class Root extends React.Component {
+  _redirectIfLoggedIn(nextState, replace) {
+    if (store.getState().session.currentUser) {
+      replace('/');
+    }
+  }
+
+  _redirectIfNotLoggedIn(nextState, replace) {
+    if (!store.getState().session.currentUser) {
+      replace('/');
+    }
+  }
+
+  render() {
+    const { store } = this.props;
+    return(
+      <Provider store={ store }>
+        <Router history={ hashHistory }>
+          <Route path="/" component={ App }>
+            <Route path="/login" component={ SessionFormContainer } onEnter={this._redirectIfLoggedIn}/>
+            <Route path="/signup" component={ SessionFormContainer } onEnter={this._redirectIfLoggedIn}/>
+          </Route>
+        </Router>
+      </Provider>
+    )
+  }
+}
 
 export default Root;
