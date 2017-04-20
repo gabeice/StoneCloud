aca5cd73e0cee7ad5101e67b42d1ee66b18e8e22
tacitblue <gabrielmcdonald1@gmail.com>
2017-04-20 16:27:50 -0400

    Play button

--- frontend/components/tracks/track_show.jsx
+++ frontend/components/tracks/track_show.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class TrackShow extends Component {
+  constructor(props) {
+    super(props)
+    this.togglePlay = this.togglePlay.bind(this);
+  }
+
   componentDidMount() {
     this.props.fetchTrack(this.props.params.trackId);
   }
@@ -11,17 +16,38 @@ class TrackShow extends Component {
     }
   }
 
+  togglePlay(e) {
+    e.preventDefault();
+    const song = $('#song')[0];
+    const buttonImage = $('.fa')[0];
+    if(song.paused) {
+      song.play();
+      buttonImage.className = "fa fa-pause";
+      buttonImage.style.fontSize = "2.5em";
+      buttonImage.style.margin = "17px 20px";
+    } else {
+      song.pause();
+      buttonImage.className = "fa fa-play";
+      buttonImage.style.fontSize = "3em";
+      buttonImage.style.margin = "14px 23px";
+    }
+  }
+
   render() {
     if(this.props.track) {
       return(
         <section className="track-show">
           <div id="track-controller">
-            <audio src={this.props.track.song_url} controls/>
+            <audio id="song" src={this.props.track.song_url}/>
+            <a href="#" id="play-button" onClick={this.togglePlay}>
+              <i className="fa fa-play" aria-hidden="true"></i>
+            </a>
 
             <div id="track-info">
               <p>{this.props.track.poster}</p>
               <h2>{this.props.track.title}</h2>
             </div>
+
           </div>
           <img src={this.props.track.image_url}/>
         </section>
