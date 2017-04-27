af8bc340a72643369c6ae76febec6a11ed5bc8de
tacitblue <gabrielmcdonald1@gmail.com>
2017-04-27 14:43:30 -0400

    Tracklist shows up next

--- frontend/components/tracks/track_index.jsx
+++ frontend/components/tracks/track_index.jsx
@@ -5,12 +5,20 @@ import Spinner from '../spinner';
 class TrackIndex extends Component {
   componentDidMount() {
     this.props.fetchTracks(this.props.search);
+    this.renderNext = this.renderNext.bind(this);
   }
 
   componentWillReceiveProps(newProps) {
     if(newProps.search != this.props.search) {
       this.props.fetchTracks(newProps.search);
     }
+  }
+
+  renderNext(song) {
+    const color = song.id === this.props.nowPlaying.id ? "blue" : "";
+    return(
+      <li key={song.id} className={color}>{song.title}</li>
+    );
   }
 
   render() {
@@ -25,14 +33,28 @@ class TrackIndex extends Component {
         searchResults = `Sorry, no results found for "${this.props.search.search}"`;
       }
 
+      let queue = [];
+      Object.keys(this.props.queue).forEach((key) => {
+        queue[key] = this.props.queue[key];
+      });
+
       return(
-        <div>
-          <u><h2 className="search-results">{searchResults}</h2></u>
-          <ul className="track-index">
-            {this.props.tracks.sort((a, b) => Date.parse(b.updated_at) - Date.parse(a.updated_at))
-              .map(track => <TrackIndexItem track={track} key={track.id}/>)}
-          </ul>
-        </div>
+        <section id="index">
+          <div>
+            <u><h2 className="search-results">{searchResults}</h2></u>
+            <ul className="track-index">
+              {this.props.tracks.sort((a, b) => Date.parse(b.updated_at) - Date.parse(a.updated_at))
+                .map(track => <TrackIndexItem track={track} key={track.id}/>)}
+            </ul>
+          </div>
+
+          <div id="tracklist">
+            <u><h3>Tracklist</h3></u>
+            <ol className="tracklist">
+              {queue.map(song => this.renderNext(song))}
+            </ol>
+          </div>
+        </section>
       );
     } else {
       return(<Spinner />);
--- frontend/components/tracks/track_index_container.js
+++ frontend/components/tracks/track_index_container.js
@@ -2,12 +2,12 @@ import { connect } from 'react-redux';
 import TrackIndex from './track_index';
 import { fetchTracks } from '../../actions/track_actions';
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    tracks: Object.values(state.tracks),
-    search: ownProps.location.query
-  };
-};
+const mapStateToProps = (state, ownProps) => ({
+  tracks: Object.values(state.tracks),
+  search: ownProps.location.query,
+  queue: state.playlist,
+  nowPlaying: state.nowPlaying
+});
 
 const mapDispatchToProps = dispatch => ({
   fetchTracks: (search) => dispatch(fetchTracks(search))
